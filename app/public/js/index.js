01e7d9e0b4202ef0202c6622678b9f296812a323
shiropt <63380875+shiropt@users.noreply.github.com>
2020-11-06 09:58:13 +0900

    add edit delete user

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -17,7 +17,21 @@ const indexModule = (() => {
         .addEventListener("click", () => {
           return window.location.href = "/"
         })
-      break;
+    case "/edit.html":
+      const uid = window.location.search.split("?uid=")[1]
+      document.getElementById("save-btn")
+        .addEventListener("click", () => {
+          return userModule.saveUser(uid)
+        })
+        document.getElementById("cancel-btn")
+        .addEventListener("click", () => {
+          return window.location.href = "/"
+        })
+        document.getElementById("delete-btn")
+          .addEventListener("click", () => {
+            return userModule.deleteUser(uid)
+          })
+       return userModule.setExistingValue(uid)
       default:
     break;
   }
--- app/public/js/users.js
+++ app/public/js/users.js
@@ -18,12 +18,12 @@ const userModule = (() => {
                         <td>${user.date_of_birth}</td>
                         <td>${user.created_at}</td>
                         <td>${user.updated_at}</td>
+                        <td><a href="edit.html?uid=${user.id}">編集</a></td>
                       </tr>`
         document.getElementById("users-list").insertAdjacentHTML('beforeend',body)
       }
     },
     createUser: async () => {
-      console.log("name");
       const name = document.getElementById("name").value
       const profile = document.getElementById("profile").value
       const dateOfBirth = document.getElementById("date-of-birth").value
@@ -40,6 +40,50 @@ const userModule = (() => {
       const resJson = await res.json()
       alert(resJson.message)
       window.location.href = "/"
+    },
+    setExistingValue: async (uid) => {
+      const res = await fetch(BASE_URL + "/" + uid)
+      const resJson = await res.json()
+
+      document.getElementById("name").value = resJson.name
+      document.getElementById("profile").value = resJson.profile
+      document.getElementById("date-of-birth").value = resJson.date_of_birth
+
+
+      
+    },
+    saveUser: async (uid) => {
+      const name = document.getElementById("name").value
+      const profile = document.getElementById("profile").value
+      const dateOfBirth = document.getElementById("date-of-birth").value
+      const body = {
+        name: name,
+        profile: profile,
+        date_of_birth:dateOfBirth
+      }
+      const res = await fetch(BASE_URL + "/" + uid, {
+        method: "PUT",
+        headers: headers,
+        body:JSON.stringify(body)
+      })
+      const resJson = await res.json()
+      alert(resJson.message)
+      window.location.href = "/"
+    },
+    deleteUser: async (uid) => {
+      const ret = window.confirm("このユーザーを削除しますか？")
+      
+      if (!ret) {
+        return false
+      } else {
+      const res = await fetch(BASE_URL + "/" + uid, {
+        method: "DELETE",
+        headers: headers
+      }) 
+        const resJson = await res.json()
+        alert(resJson.message)
+        window.location.href = "/"
+      }
     }
   }
 })()
